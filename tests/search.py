d413cd62a773576aa7a1445af4e4106698732a8a
Martin Lenders <mail@martin-lenders.de>
2013-03-31 17:31:58 +0200

    Rework search test

--- tests/search.py
+++ tests/search.py
@@ -1,14 +1,21 @@
 import pycomicvine
 import unittest
 
-class TestSearch(unittest.TestCase):
-    def setUp(self):
-        pycomicvine.api_key = "476302e62d7e8f8f140182e36aebff2fe935514b"
+api_key = "476302e62d7e8f8f140182e36aebff2fe935514b"
 
+class TestSearch(unittest.TestCase):
     def test_search_resource_type(self):
         search = pycomicvine.Search(
                 resources="volume", 
                 query="Angel"
             )
-        self.assertTrue(isinstance(search[0], pycomicvine.Volume))
+        self.assertIsInstance(search[0], pycomicvine.Volume)
+
+    def test_search_id(self):
+        search = pycomicvine.Search(
+                query="The Walking Dead",
+                field_list=["id"]
+            )
+        self.assertNotEqual(len(search),0)
+        self.assertEqual(search[0].id, 18166)
 
