a700377f9560d532cae228aebfbc36d52b475619
Martin Lenders <mail@martin-lenders.de>
2013-03-31 12:03:05 +0200

    Add test driver

--- setup.py
+++ setup.py
@@ -1,11 +1,35 @@
 #!/usr/bin/env python
 from setuptools import setup
+from distutils.core import Command
 
 VERSION = '0.8'
 DESCRIPTION = "A wrapper for comicvine.com"
 
 with open('README.md', 'r') as f:
    LONG_DESCRIPTION = f.read()
+
+class TestCommand(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import sys, subprocess
+        raise SystemExit(
+                subprocess.call([
+                        sys.executable,
+                        # Turn on deprecation warnings
+                        '-Wd',
+                        # Use module unittest
+                        '-m',
+                        'unittest',
+                        'tests'
+                    ])
+            )
 
 setup(
         name='PyComicVine',
@@ -17,5 +41,6 @@ setup(
         url='http://www.github.com/authmillenon/pycomicvine/',
         packages=['pycomicvine'],
         install_requires=['simplejson','python-dateutil >= 2.0'],
-        license="MIT License"
+        license="MIT License",
+        cmdclass={'test': TestCommand}
     )
